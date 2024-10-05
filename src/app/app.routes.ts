e82c4840f201564475898a06831d5fa4cac94d32
kayehol <kaye.holanda@gmail.com>
2024-10-04 21:16:08 -0300

    adiciona rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UnitComponent } from './unit/unit.component';
+import { NewUnitComponent } from './new-unit/new-unit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cnes/estabelecimentos',
+    title: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'cnes/estabelecimentos/:codigo',
+    title: 'Unidade',
+    component: UnitComponent,
+  },
+  {
+    path: 'cnes/nova-unidade',
+    title: 'Nova unidade',
+    component: NewUnitComponent,
+  },
+];
