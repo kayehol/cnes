27c80eb9a0de0a0b3b68b0a7d591abb1d0be4aa8
kayehol <kaye.holanda@gmail.com>
2024-10-05 23:08:16 -0300

    exibe formulario de cadastro

--- src/app/new-unit/new-unit.component.ts
+++ src/app/new-unit/new-unit.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-new-unit',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './new-unit.component.html',
   styleUrl: './new-unit.component.css'
 })
 export class NewUnitComponent {
+  unitForm = new FormGroup({
+    codigoCnes: new FormControl(''),
+    nomeRazaoSocial: new FormControl(''),
+    nomeFantasia: new FormControl(''),
+    cnpj: new FormControl(''),
+    telefone: new FormControl(''),
+    email: new FormControl(''),
+    codigoEstabelecimento: new FormControl(''),
+    descricaoTurno: new FormControl(''),
+    endereco: new FormControl(''),
+    numero: new FormControl(''),
+    bairro: new FormControl(''),
+    municipio: new FormControl(''),
+    uf: new FormControl(''),
+    cep: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.unitForm.value);
+  }
 
 }
